0e226c82e5026472d737f0f1ad279335a6610c91
Rayan <Rayan_elmorabet@hotmail.com>
2025-11-29 21:25:21 +0100

    it works

--- eip1559_tx.js
+++ eip1559_tx.js
@@ -5,8 +5,7 @@
 import {ethers} from "ethers";
 
 // .env file
-
-const { loadEnvFile } = require('node:process');
+import {loadEnvFile} from "node:process";
 
 loadEnvFile();
 
@@ -35,5 +34,36 @@ const txData = {
 		data: "0x", // Optional data
 		chainId: 11155111, //sepolia
 };
+
+// Calculate the RLP-encoded tx hash -> before signing
+
+const unsignedTx = ethers.Transaction.from(txData).unsignedSerialized;
+
+console.log("RLP-Encoded Tx (Unsigned): " + unsignedTx);
+
+const txHash = ethers.keccak256(unsignedTx);
+
+console.log("Tx Hash (Unsigned): " + txHash);
+
+// Sign the tx
+
+async function signAndSend(){
+		
+		// Sign the tx with the wallet
+		const signedTx = await wallet.signTransaction(txData);
+		console.log("Signed Raw Transaction: " + signedTx);
+		
+		// Send the signed tx to the ETH network
+		const txResponse = await provider.broadcastTransaction(signedTx);
+		console.log("Transaction Hash: " + txResponse.hash);
+
+		// wait for the tx to be mined
+		const receipt = await txResponse.wait();
+		console.log("Transaction Receipt: ", receipt);
+
+}
+
+signAndSend().catch(console.error);
+		
 
 
